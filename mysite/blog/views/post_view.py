aa5f7cced23914ed1824eeda798361f67f128427
Nicolas <nicolas.nd@hotmail.com>
2026-09-25 20:13:33 -0300

    feat: adiciona view de post com Hello World e testes

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -2,6 +2,5 @@ from django.http import HttpResponse
 from django.views import generic
 
 class PostView(generic.View):
-
     def get(self, request, *args, **kwargs):
         return HttpResponse('Hello World!')
